36b4ac9f413ca208b8c0673fc3640ea927a5d946
yonathan <yonathantambani109@gmail.com>
2025-09-18 10:38:48 +0700

    Remove registration_types and reservations APIs, update forms and frontend services

--- hotel-python-backend/app/api/__init__.py
+++ hotel-python-backend/app/api/__init__.py
@@ -1,1 +1,11 @@
 # API Routes
+from . import auth
+from . import rooms  
+from . import guests
+from . import hotel_registrations
+from . import hotel_reservations
+from . import cities
+from . import nationalities
+from . import category_markets
+from . import market_segments
+from . import payment_methods
